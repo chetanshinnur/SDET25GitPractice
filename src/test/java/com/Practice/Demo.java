ebca194ffa69bc116bee5c31214ff4e6a079f26c
VeereshHitnal <veereshhitnal@gmail.com>
2021-12-09 13:48:07 +0530

    Demo test created

---
+++ src/test/java/com/Practice/Demo.java
@@ -0,0 +1,12 @@
+package com.Practice;
+
+import org.testng.annotations.Test;
+
+public class Demo {
+	@Test
+	
+	public void demoTest() {
+		System.out.println("this is User 1 - script -1 ");
+	}
+
+}
