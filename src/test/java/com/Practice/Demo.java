d665aca905e18ac1421fd9842236d4329f97e987
VeereshHitnal <veereshhitnal@gmail.com>
2021-12-10 09:50:55 +0530

    commit 2

--- src/test/java/com/Practice/Demo.java
+++ src/test/java/com/Practice/Demo.java
@@ -9,6 +9,9 @@ public class Demo {
 		System.out.println("this is User 1 - script -1 ");
 		System.out.println("this is User 2 - script  2 ");
 		System.out.println("this is User 2 - script  3 ");
+		System.out.println("this is User 2 - script  4 ");
+		System.out.println("this is User 2 - script  5 ");
+
 
 	}
 
